357083b2e3a4c6966740fd2a406e12f4d80f6e53
Andres Hernandez <andres.hernandez@utexas.edu>
2020-09-25 21:45:58 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,8 @@
 def square_array(array)
-  # your code here
+  array(1,2,3,4,5)
+  new_array()
+  counter = 0
+  array.each do |number|
+    new_array[counter] = number*number
+  return new_array
 end
